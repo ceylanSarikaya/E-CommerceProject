39f9c4c6ffa3814096f868783840db1fa4df4e88
Ceylan Sarıkaya <77547822+srky974@users.noreply.github.com>
2022-08-02 22:26:30 +0300

    Add project files.

---
+++ Entity/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public string Description { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
